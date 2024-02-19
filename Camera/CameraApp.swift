86760c9b92b8ef80453e21441ef88d405b21b3a3
Siam300 <89306483+Siam300@users.noreply.github.com>
2024-02-19 20:41:32 +0600

    Initial Commit

---
+++ Camera/CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraApp.swift
+//  Camera
+//
+//  Created by Auto on 19/2/24.
+//
+
+import SwiftUI
+
+@main
+struct CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Camera/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Camera
+//
+//  Created by Auto on 19/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
